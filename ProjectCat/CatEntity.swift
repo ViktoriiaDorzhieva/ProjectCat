b4414d0bba1af55f021910a3ce95cf719e2b300f
Viktoriia Dorzhieva <viktoria1dorzhieva@gmail.com>
2024-05-17 21:24:46 -0700

    Swap base colors

--- ProjectCat/CatEntity.swift
+++ ProjectCat/CatEntity.swift
@@ -23,8 +23,14 @@ class CatEntity: Entity, HasModel {
     var catState = CatStates.idle
     var idleTimer: Double = 0
     var walkTimer: Double = 0
-    var textureCatWalking : PhysicallyBasedMaterial.Texture? = nil
-    var textureCatIdle : PhysicallyBasedMaterial.Texture? = nil
+    
+    var baseColorIdle: UnlitMaterial.BaseColor? = nil
+    var baseColorWalking: UnlitMaterial.BaseColor? = nil
+    
+    var simpleMaterial = UnlitMaterial()
+    var randomDouble: Double = 0
+    var randomX:Float = 0
+    var randomZ:Float = 0
     
     required init() {
         super.init()
@@ -39,39 +45,44 @@ class CatEntity: Entity, HasModel {
         
         let indicesArray = Array(0..<meshVertices.count)
         meshDescriptor.primitives = .triangles(indicesArray.map { UInt32($0)})
+       
         guard let meshResource = try? MeshResource.generate(from: [meshDescriptor]) else {
             fatalError("Error creating the mesh")
-//            return
         }
         
-        guard let textureResourceCatWalking = try? TextureResource.load(named: "catWalking") else {
+        guard let textureResourceCatIdle = try? TextureResource.load(named: "catStanding"),
+              let textureResourceCatWalking = try? TextureResource.load(named: "catWalking")
+        else {
             return
         }
-        textureCatWalking = PhysicallyBasedMaterial.Texture(textureResourceCatWalking)
         
-        // init idle texture
+        let textureCatWalking = PhysicallyBasedMaterial.Texture(textureResourceCatWalking)
+        let textureCatIdle = PhysicallyBasedMaterial.Texture(textureResourceCatIdle)
         
-            //        var simpleMaterial = SimpleMaterial(color: .red, isMetallic: false)
-        var simpleMaterial = UnlitMaterial()
-        simpleMaterial.color = PhysicallyBasedMaterial.BaseColor(texture: textureCatWalking)
+        self.baseColorIdle = UnlitMaterial.BaseColor(texture: textureCatIdle)
+        self.baseColorWalking = UnlitMaterial.BaseColor(texture: textureCatWalking)
+
+        guard let baseColorIdle = self.baseColorIdle else {
+            return
+        }
+        self.simpleMaterial.color = baseColorIdle
+
         self.model = ModelComponent(mesh: meshResource, materials: [simpleMaterial])
         self.components[CatComponent.self] = .init()
         
         self.transform.translation.y = 0.05
         self.transform.scale = simd_make_float3(0.1, 0.1, 0.1)
         
-//        let transform = Transform(scale: .one, rotation: simd_quatf(), translation: [0.5, 0, 0])
-//        self.move(to: transform, relativeTo: nil, duration: 10, timingFunction: .easeInOut)
-        
-        }
+    }
     
     
     func update(deltaTime: TimeInterval) {
+        randomDouble = Double.random(in: 3..<10)
         // Put all the update behavior per frame
             switch (catState) {
             case .idle:
                 idleTimer+=deltaTime
-                if idleTimer>5 {
+                if idleTimer>randomDouble  {
                     walk()
                     idleTimer = 0
                 }
@@ -79,7 +90,7 @@ class CatEntity: Entity, HasModel {
                 
             case .walking:
                 walkTimer+=deltaTime
-                if walkTimer>5 {
+                if walkTimer>randomDouble {
                     startIdle()
                     walkTimer = 0
                 }
@@ -88,23 +99,34 @@ class CatEntity: Entity, HasModel {
         }
     
         func walk() {
+            randomX = Float.random(in: -2..<2)
+            randomZ = Float.random(in: -2..<2)
+            
             catState = .walking
             //        to make it move
             var transform = Transform()
-            transform.translation.x = 0.5
+
+            transform.translation = SIMD3<Float>(x: randomX, y: 0, z: randomZ)
+          
             
-            self.move(to: transform, relativeTo: self, duration: 5.0)
+            self.move(to: transform, relativeTo: self, duration: randomDouble)
             
-            // here swap the texture :
-            // - use the material on the entity (store it in a variable at initialization)
-            // - assign the new texture to the material color
-            //
+            guard let baseColorWalking = self.baseColorWalking else {
+                return
+            }
+            self.simpleMaterial.color = baseColorWalking
+            self.model?.materials = [simpleMaterial]
+            
         }
     
         func startIdle() {
             catState = . idle
             
-            // swap the texture
+            guard let baseColorIdle = self.baseColorIdle else {
+                return
+            }
+            self.simpleMaterial.color = baseColorIdle
+            self.model?.materials = [simpleMaterial]
             
         }
     }
@@ -141,50 +163,36 @@ class CatSystem : System {
     }
 }
 
-///// The component that marks an entity as a billboard object which will always face the camera.
+/// The component that marks an entity as a billboard object which will always face the camera.
 //public struct BillboardComponent: Component, Codable {
 //    public init() {
 //    }
 //}
-//
+
 ///// An ECS system that points all entities containing a billboard component at the camera.
 //public struct BillboardSystem: System {
 //    
-//    static let query = EntityQuery(where: .has(cat.BillboardComponent.self))
+//    static let query = EntityQuery(where: .has(BillboardComponent.self))
 //    
-//    private let arkitSession = ARKitSession()
-//    private let worldTrackingProvider = WorldTrackingProvider()
 //    
 //    public init(scene: RealityKit.Scene) {
-//        setUpSession()
+//        
 //    }
 //    
-//    func setUpSession() {
-//        
-//        Task {
-//            do {
-//                try await arkitSession.run([worldTrackingProvider])
-//            } catch {
-//                os_log(.info, "Error: \(error)")
-//            }
-//        }
-//    }
+//  
 //    
 //    public func update(context: SceneUpdateContext) {
 //        
 //        let entities = context.scene.performQuery(Self.query).map({ $0 })
 //        
-//        guard !entities.isEmpty,
-//              let pose = worldTrackingProvider.queryDeviceAnchor(atTimestamp: CACurrentMediaTime()) else { return }
-//        
-//        let cameraTransform = Transform(matrix: pose.originFromAnchorTransform)
-//        
-//        for entity in entities {
-//            entity.look(at: cameraTransform.translation,
-//                        from: entity.scenePosition,
+//    
+//        for cat in entities {
+//            cat.look(at: cameraTransform.translation,
+//                        from: cat.scenePosition,
 //                        relativeTo: nil,
 //                        forward: .positiveZ)
 //        }
 //    }
 //}
 //
+
